4fe37c6ae46d8516bd9f0bd10f4c2cffb756cc10
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-11-02 06:35:26 -0800

    Commiting changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Links from "./components/Links";
 
 const App = () => {
   return (
     <div>
       <h1>App</h1>
+      <Links />
     </div>
   );
 };
