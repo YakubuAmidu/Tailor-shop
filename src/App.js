dc98b81b89ae018aba6debc74fb5510b193306fb
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-11-02 15:56:41 -0800

    Commiting changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import Links from "./components/Links";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <div>
       <h1>App</h1>
       <Links />
+      <Header />
     </div>
   );
 };
--- src/components/Links.js
+++ src/components/Links.js
@@ -1,11 +1,49 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { Navbar, Nav } from "react-bootstrap";
 
 class Links extends Component {
   render() {
     return (
-      <div>
-        <h1>Links</h1>
+      <div className="Navbar">
+        <Navbar bg="dark" expand="md">
+          <Navbar.Brand href="#home">Tailer Shop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link>
+                <Link to="/#" className="Link_home">
+                  Home
+                </Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to="/#" className="Link_product">
+                  Product
+                </Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to="/#" className="Link_services">
+                  Services
+                </Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to="/#" className="Link_blog">
+                  Blog
+                </Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to="/#" className="Link_about">
+                  About
+                </Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to="/#" className="Link_contact">
+                  Contact
+                </Link>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
       </div>
     );
   }
