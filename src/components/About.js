cc3d97bf1db58c9d34eb498da6e04485e52fc38b
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-11-03 04:17:27 -0800

    Commiting changes

--- src/components/About.js
+++ src/components/About.js
@@ -7,63 +7,71 @@ import SuitFive from "../assets/Suit_five.png";
 class About extends Component {
   render() {
     return (
-      <Row className="About_section">
+      <div className="About_section">
         <h1 className="About_header">About</h1>
-        <Col className="About_title">
-          <p className="Paragraph_title">
-            We are Makers of custom handmade Tailored suits
-          </p>
-          <div div className="Image_title">
+        <p className="Paragraph_title">
+          We are Makers of custom handmade Tailored suits
+        </p>
+        <Row>
+          <Col className="About_title" sm={4}>
             <img src={SuitThree} alt="Suit-img" className="Image_three" />
             <p className="Title_header">HANDMADE</p>
             <p className="Title_paragraph">
               This is just a place holder, so you can see what the site would
               look like.
             </p>
-          </div>
-          <div className="Image_title">
+          </Col>
+          <Col className="About_title" sm={4}>
             <img src={SuitFive} alt="Suit-img" className="Image_three" />
             <p className="Title_header">HANDMADE</p>
             <p className="Title_paragraph">
               This is just a place holder, so you can see what the site would
               look like.
             </p>
-          </div>
-          <div className="Image_title">
+          </Col>
+          <Col className="About_title" sm={4}>
             <img src={SuitFour} alt="Suit_img" className="Image_three" />
             <p className="Title_header">HANDMADE</p>
             <p className="Title_paragraph">
               This is just a place holder, so you can see what the site would
               look like.
             </p>
-          </div>
-        </Col>
+          </Col>
+        </Row>
 
-        <Col>
+        <div className="Blog_section">
           <p className="Blog_header">Blog</p>
-          <div className="Blog_title">
-            <p className="Blog_date">May 1, 2012 posted by: Jo</p>
-            <p className="Blot_detail">
-              This is just a place holder. This is just a place holder. This is
-              just a place holder. - read more
-            </p>
-          </div>
-          <div className="Blog_title">
-            <p className="Blog_date">May 1, 2012 posted by: Jo</p>
-            <p className="Blot_detail">
-              This is just a place holder. This is just a place holder. This is
-              just a place holder. - read more
-            </p>
-          </div>
-          <div className="Blog_title">
-            <p className="Blog_date">May 1, 2012 posted by: Jo</p>
-            <p lassName="Blot_detail">
-              This is just a place holder. This is just a place holder. This is
-              just a place holder. - read more
-            </p>
-          </div>
-        </Col>
-      </Row>
+          <Row>
+            <Col className="Blog_title" sm={4}>
+              <div className="Blog_title">
+                <p className="Blog_date">May 1, 2012 posted by: Jo</p>
+                <p className="Blot_detail">
+                  This is just a place holder. This is just a place holder. This
+                  is just a place holder. - read more
+                </p>
+              </div>
+            </Col>
+            <Col className="Blog_title" sm={4}>
+              <div className="Blog_title">
+                <p className="Blog_date">May 1, 2012 posted by: Jo</p>
+                <p className="Blot_detail">
+                  This is just a place holder. This is just a place holder. This
+                  is just a place holder. - read more
+                </p>
+              </div>
+            </Col>
+            <Col className="Blog_title" sm={4}>
+              <div className="Blog_title">
+                <p className="Blog_date">May 1, 2012 posted by: Jo</p>
+                <p lassName="Blot_detail">
+                  This is just a place holder. This is just a place holder. This
+                  is just a place holder. - read more
+                </p>
+              </div>
+            </Col>
+          </Row>
+        </div>
+      </div>
     );
   }
 }
