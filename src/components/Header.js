308bb94a321807303241fbcf5e47522a6b1b8cd3
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-11-03 00:55:13 -0800

    Commiting changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,36 +7,27 @@ import Suit_two from "../assets/Suit_two.png";
 class Header extends Component {
   render() {
     return (
-      <div className="Header_section">
-        <Row>
-          <Col>
-            <div className="Header_info">
-              <h2>World's Finest and Elegant Tuxedo</h2>
-              <p>
-                This is just a place holder, so you can see what the site would
-                look like. This is just a place holder, so you can see what the
-                site would look like. This is just a place holder, so you can
-                see what the site would look like. This is just a place holder,
-                so you can see what the site would look like.
-              </p>
-              <Button variant="outline-success">Shop Now</Button>
-            </div>
+      <div>
+        <Row className="Header_section">
+          <Col className="Header_info">
+            <h2>World's Finest and Elegant Tuxedo</h2>
+            <p>
+              This is just a place holder, so you can see what the site would
+              look like. This is just a place holder, so you can see what the
+              site would look like. This is just a place holder, so you can see
+              what the site would look like. This is just a place holder, so you
+              can see what the site would look like.
+            </p>
+            <Button variant="outline-success">Shop Now</Button>
           </Col>
-          <Col>
-            <div className="Header_image">
-              <img src={Suit} alt="suit-img" className="Image_one" />
-            </div>
+          <Col className="Header_image">
+            <img src={Suit} alt="suit-img" className="Image_one" />
           </Col>
-          <Col>
-            <div className="Header_image-one">
-              <p className="Header_title">
-                Handcrafted and made from 100% wool
-              </p>
-              <div className="header_image-two">
-                <img src={Suit_one} alt="suit-img" className="Image_two" />
-                <img src={Suit_two} alt="suit-img" className="Image_two" />
-              </div>
-            </div>
+          <Col className="Header_image-one">
+            <p className="Header_title">Handcrafted and made from 100% wool</p>
+
+            <img src={Suit_one} alt="suit-img" className="Image_two" />
+            <img src={Suit_two} alt="suit-img" className="Image_two" />
           </Col>
         </Row>
       </div>
