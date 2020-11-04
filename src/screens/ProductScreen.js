d0fac5031ea5a37638956af3ce3835dc1e53b377
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-11-04 01:52:24 -0800

    Commiting changes

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,9 +1,35 @@
 import React from "react";
+import Menone from "../assets/Menone.png";
+import Mentwo from "../assets/Mentwo.png";
+import Menthree from "../assets/Menthree.png";
+import Menfour from "../assets/Menfour.png";
 
 const ProductScreen = () => {
   return (
-    <div>
-      <h1>Product</h1>
+    <div className="Product_section">
+      <p>World's finest and elegant tuxedo's</p>
+      <div>
+        <div>
+          <img src={Menone} alt="img" />
+          <h4>product name</h4>
+          <p>$1,000.00</p>
+        </div>
+        <div>
+          <img src={Mentwo} alt="img" />
+          <h4>product name</h4>
+          <p>$1,000.00</p>
+        </div>
+        <div>
+          <img src={Menthree} alt="img" />
+          <h4>product name</h4>
+          <p>$1,000.00</p>
+        </div>
+        <div>
+          <img src={Menfour} alt="img" />
+          <h4>product name</h4>
+          <p>$1,000.00</p>
+        </div>
+      </div>
     </div>
   );
 };
