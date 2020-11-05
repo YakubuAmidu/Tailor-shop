ea6cce80a54d75a2d936aadb072d59fbd97c3f03
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-11-05 06:52:56 -0800

    Commiting changes

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,38 +1,38 @@
 import React from "react";
+import { Col, Row } from "react-bootstrap";
 import Service from "../components/Service";
 import Links from "../components/Links";
 import Menone from "../assets/Menone.png";
 import Mentwo from "../assets/Mentwo.png";
 import Menthree from "../assets/Menthree.png";
 import Menfour from "../assets/Menfour.png";
-import { Col, Row } from "react-bootstrap";
 
 const ProductScreen = () => {
   return (
     <div className="Product_section">
       <Links />
-      <p className="Suit_header">World's finest and elegant tuxedo's</p>
+      <p className="Product_header">World's finest and elegant tuxedo's</p>
       <Row>
         <Col sm={6}>
           <div className="Suit_image">
-            <img src={Menone} alt="img" className="Suit_img" />
-            <h4 className="Suit_title">product name</h4>
+            <img src={Menone} alt="img" className="Suit_img-one" />
+            <h4 className="Suit_name">product name</h4>
             <p className="Suit_price">$1,000.00</p>
           </div>
           <div className="Suit_image">
-            <img src={Mentwo} alt="img" className="Suit_img" />
-            <h4 className="Suit_title">product name</h4>
+            <img src={Mentwo} alt="img" className="Suit_img-one" />
+            <h4 className="Suit_name">product name</h4>
             <p className="Suit_price">$1,000.00</p>
           </div>
         </Col>
         <Col sm={6}>
           <div className="Suit_image">
-            <img src={Menthree} alt="img" className="Suit_img" />
-            <h4 className="Suit_title">product name</h4>
+            <img src={Menthree} alt="img" className="Suit_img-one" />
+            <h4 className="Suit_name">product name</h4>
             <p className="Suit_price">$1,000.00</p>
           </div>
           <div className="Suit_image">
-            <img src={Menfour} alt="img" className="Suit_img" />
+            <img src={Menfour} alt="img" className="Suit_img-one" />
             <h4 className="Suit_title">product name</h4>
             <p className="Suit_price">$1,000.00</p>
           </div>
