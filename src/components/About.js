473a169954af4b45f0040255ceed5beea46a5814
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-11-04 22:10:38 -0800

    Commiting changes

--- src/components/About.js
+++ src/components/About.js
@@ -13,55 +13,67 @@ class About extends Component {
           We are Makers of custom handmade Tailored suits
         </p>
         <Row>
-          <Col className="About_title" sm={4}>
-            <img src={SuitThree} alt="Suit-img" className="Image_three" />
-            <p className="Title_header">HANDMADE</p>
-            <p className="Title_paragraph">
-              This is just a place holder, so you can see what the site would
-              look like.
-            </p>
+          <Col sm={4}>
+            <div className="About_title">
+              <img src={SuitThree} alt="Suit-img" className="Image_three" />
+              <p className="Title_header">HANDMADE</p>
+              <p className="Title_paragraph">
+                This is just a place holder, so you can see what the site would
+                look like.
+              </p>
+            </div>
           </Col>
-          <Col className="About_title" sm={4}>
-            <img src={SuitFive} alt="Suit-img" className="Image_three" />
-            <p className="Title_header">DESIGNS</p>
-            <p className="Title_paragraph">
-              This is just a place holder, so you can see what the site would
-              look like.
-            </p>
+          <Col sm={4}>
+            <div className="About_title">
+              <img src={SuitFive} alt="Suit-img" className="Image_three" />
+              <p className="Title_header">DESIGNS</p>
+              <p className="Title_paragraph">
+                This is just a place holder, so you can see what the site would
+                look like.
+              </p>
+            </div>
           </Col>
-          <Col className="About_title" sm={4}>
-            <img src={SuitFour} alt="Suit_img" className="Image_three" />
-            <p className="Title_header">PERFECT FIT</p>
-            <p className="Title_paragraph">
-              This is just a place holder, so you can see what the site would
-              look like.
-            </p>
+          <Col sm={4}>
+            <div className="About_title">
+              <img src={SuitFour} alt="Suit_img" className="Image_three" />
+              <p className="Title_header">PERFECT FIT</p>
+              <p className="Title_paragraph">
+                This is just a place holder, so you can see what the site would
+                look like.
+              </p>
+            </div>
           </Col>
         </Row>
 
         <div className="Blog_section">
           <p className="Blog_header">Blog</p>
           <Row>
-            <Col className="Blog_title" sm={4}>
-              <p className="Blog_date">May 1, 2012 posted by: Jo</p>
-              <p className="Blog_detail">
-                This is just a place holder. This is just a place holder. This
-                is just a place holder. - read more
-              </p>
+            <Col sm={4}>
+              <div className="Blog_title">
+                <p className="Blog_date">May 1, 2012 posted by: Jo</p>
+                <p className="Blog_detail">
+                  This is just a place holder. This is just a place holder. This
+                  is just a place holder. - read more
+                </p>
+              </div>
             </Col>
-            <Col className="Blog_title" sm={4}>
-              <p className="Blog_date">May 1, 2012 posted by: Jo</p>
-              <p className="Blog_detail">
-                This is just a place holder. This is just a place holder. This
-                is just a place holder. - read more
-              </p>
+            <Col sm={4}>
+              <div className="Blog_title">
+                <p className="Blog_date">May 1, 2012 posted by: Jo</p>
+                <p className="Blog_detail">
+                  This is just a place holder. This is just a place holder. This
+                  is just a place holder. - read more
+                </p>
+              </div>
             </Col>
-            <Col className="Blog_title" sm={4}>
-              <p className="Blog_date">May 1, 2012 posted by: Jo</p>
-              <p lassName="Blog_detail">
-                This is just a place holder. This is just a place holder. This
-                is just a place holder. - read more
-              </p>
+            <Col sm={4}>
+              <div className="Blog_title">
+                <p className="Blog_date">May 1, 2012 posted by: Jo</p>
+                <p lassName="Blog_detail">
+                  This is just a place holder. This is just a place holder. This
+                  is just a place holder. - read more
+                </p>
+              </div>
             </Col>
           </Row>
         </div>
--- src/screens/ServicesScreen.js
+++ src/screens/ServicesScreen.js
@@ -12,73 +12,89 @@ const ServicesScreen = () => {
     <div>
       <Links />
       <div className="Service_section-main">
-        <h1 className="Servicescreen">ServicesScreen</h1>
-        <Row className="Service_main">
-          <Col className="Service_image" sm={4}>
-            <img src={Measureone} alt="img" className="Measureone" />
+        <h1 className="Services_screen">Services</h1>
+        <Row>
+          <Col sm={4}>
+            <div className="Service_image">
+              <img src={Measureone} alt="img" className="Measureone" />
+            </div>
           </Col>
-          <Col className="Measure_paragraph" sm={8}>
-            <p className="Measure_info">
-              This website template has been designed by Free Website Templates
-              for you, for free. You can replace all this text with your own
-              text. Want an easier solution for a Free Website? Head straight to
-              Wix and immediately start customizing your website! Wix is an
-              online website builder with a simple drag & drop interface,
-              meaning you do the work online and instantly publish to the web.
-              Nothing to download,
-            </p>
+          <Col sm={8}>
+            <div className="Measure_main">
+              <p className="Measure_paragraph">
+                This website template has been designed by Free Website
+                Templates for you, for free. You can replace all this text with
+                your own text. Want an easier solution for a Free Website? Head
+                straight to Wix and immediately start customizing your website!
+                Wix is an online website builder with a simple drag & drop
+                interface, meaning you do the work online and instantly publish
+                to the web. Nothing to download,
+              </p>
+            </div>
           </Col>
         </Row>
         <br />
         <Row>
           <Col sm={4}>
-            <img src={Measurethree} alt="img" className="Measuretwo" />
-            <p>
-              Wix also offers a ton of free design elements right inside the
-              editor, like images, icons, animation files, and interactive
-              widgets. Publish your Free Website in minutes!
-            </p>
+            <div className="Measure_paragraph-one">
+              <img src={Measurethree} alt="img" className="Measuretwo" />
+              <p className="Measure_paragraph-two">
+                Wix also offers a ton of free design elements right inside the
+                editor, like images, icons, animation files, and interactive
+                widgets. Publish your Free Website in minutes!
+              </p>
+            </div>
           </Col>
           <Col sm={4}>
-            <img src={Measurefour} alt="img" className="Measuretwo" />
-            <p>
-              Wix also offers a ton of free design elements right inside the
-              editor, like images, icons, animation files, and interactive
-              widgets. Publish your Free Website in minutes!
-            </p>
+            <div className="Measure_paragraph-one">
+              <img src={Measurefour} alt="img" className="Measuretwo" />
+              <p className="Measure_paragraph-two">
+                Wix also offers a ton of free design elements right inside the
+                editor, like images, icons, animation files, and interactive
+                widgets. Publish your Free Website in minutes!
+              </p>
+            </div>
           </Col>
           <Col sm={4}>
-            <img src={Measuretwo} alt="img" className="Measuretwo" />
-            <p>
-              Wix also offers a ton of free design elements right inside the
-              editor, like images, icons, animation files, and interactive
-              widgets. Publish your Free Website in minutes!
-            </p>
+            <div className="Measure_paragraph-one">
+              <img src={Measuretwo} alt="img" className="Measuretwo" />
+              <p className="Measure_paragraph-two">
+                Wix also offers a ton of free design elements right inside the
+                editor, like images, icons, animation files, and interactive
+                widgets. Publish your Free Website in minutes!
+              </p>
+            </div>
           </Col>
         </Row>
         <Row>
           <Col sm={4}>
-            <p>
-              For appointment
-              <br />
-              This is just a place holder, so you can see what the site would
-              look like.
-            </p>
+            <div className="Appointment_header">
+              <p className="Appointment">
+                For appointment
+                <br />
+                This is just a place holder, so you can see what the site would
+                look like.
+              </p>
+            </div>
           </Col>
           <Col sm={4}>
-            <p>
-              Call: 987 654 3210 765 432 1098
-              <br />
-              Email: company@emailcom
-            </p>
+            <div className="Number_header">
+              <p className="Number">
+                Call: 987 654 3210 765 432 1098
+                <br />
+                Email: company@emailcom
+              </p>
+            </div>
           </Col>
           <Col sm={4}>
-            <p>
-              Location:
-              <br />
-              126-U This is just a place holder, so you can see what the site
-              would look like., 9183
-            </p>
+            <div className="Location_header">
+              <p className="Location">
+                Location:
+                <br />
+                126-U This is just a place holder, so you can see what the site
+                would look like., 9183
+              </p>
+            </div>
           </Col>
         </Row>
         <Service />
