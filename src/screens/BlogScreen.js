8e69dd4508c739f2ace425ab7e5024d61aa7b030
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-11-05 00:17:56 -0800

    Commiting changes

--- src/screens/BlogScreen.js
+++ src/screens/BlogScreen.js
@@ -3,6 +3,8 @@ import Links from "../components/Links";
 import { Col, Row } from "react-bootstrap";
 import Brideone from "../assets/Brideone.png";
 import Bridetwo from "../assets/Bridetwo.png";
+import Tailorone from "../assets/Tailorone.png";
+import Service from "../components/Service";
 
 const Blog = () => {
   return (
@@ -32,6 +34,76 @@ const Blog = () => {
           </div>
         </Col>
       </Row>
+
+      <div>
+        <Row>
+          <Col>
+            <div>
+              <p>CATEGORIES:</p>
+              <ul>
+                <li>This is just a place holder.</li>
+                <li>This is just a place holder.</li>
+                <li>This is just a place holder.</li>
+                <li>This is just a place holder.</li>
+                <li>This is just a place holder.</li>
+                <li>This is just a place holder.</li>
+              </ul>
+            </div>
+          </Col>
+          <Col>
+            <div>
+              <p>CATEGORIES:</p>
+              <ul>
+                <li>This is just a place holder.</li>
+                <li>This is just a place holder.</li>
+                <li>This is just a place holder.</li>
+                <li>This is just a place holder.</li>
+                <li>This is just a place holder.</li>
+                <li>This is just a place holder.</li>
+              </ul>
+            </div>
+          </Col>
+          <Col>
+            <div>
+              <p>CATEGORIES:</p>
+              <ul>
+                <li>This is just a place holder.</li>
+                <li>This is just a place holder.</li>
+                <li>This is just a place holder.</li>
+                <li>This is just a place holder.</li>
+                <li>This is just a place holder.</li>
+                <li>This is just a place holder.</li>
+              </ul>
+            </div>
+          </Col>
+        </Row>
+      </div>
+
+      <div>
+        <Row>
+          <Col>
+            <div>
+              <p>
+                May 6, 2012 posted by: Mark This is just a place holder. This is
+                just a place holder, so you can see what the site would look
+                like. This is just a place holder, so you can see what the site
+                would look like. This is just a place holder, so you can see
+                what the site would look like. This is just a place holder, so
+                you can see what the site would look like. This is just a place
+                holder, so you can see what the site would look like. This is
+                just a place holder, so you can see what the site would look
+                like. - read more
+              </p>
+            </div>
+          </Col>
+          <Col>
+            <div>
+              <img src={Tailorone} alt="img" />
+            </div>
+          </Col>
+        </Row>
+      </div>
+      <Service />
     </div>
   );
 };
