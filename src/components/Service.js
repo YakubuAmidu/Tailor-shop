806e3b75734175a61b8a1e503d0c363830c9c557
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-11-05 04:12:16 -0800

    Commiting changes

--- src/components/Service.js
+++ src/components/Service.js
@@ -6,27 +6,30 @@ class Service extends Component {
     return (
       <div className="Service_section">
         <Row>
-          <Col className="Service">
-            <h1 className="Service_title">Sercvices</h1>
-            <p className="Service_paragraph">
-              This website template has been designed by Free Website Templates
-              for you, for free. You can replace all this text with your own
-              text. You can remove any link to our website from this website
-              template, you're free to use this website template without linking
-              back to us. If you're having problems editing this website
-              template, then don't hesitate to ask for help on the Forums.
-            </p>
+          <Col>
+            <div className="Service_header">
+              <h1 className="Service_title">Sercvices</h1>
+              <p className="Service_paragraph">
+                This website template has been designed by Free Website
+                Templates for you, for free. You can replace all this text with
+                your own text. You can remove any link to our website from this
+                website template, you're free to use this website template
+                without linking back to us. If you're having problems editing
+                this website template, then don't hesitate to ask for help on
+                the Forums.
+              </p>
 
-            <div className="Social_icons">
-              <h5 className="Connect">Connect with me</h5>
-              <i class="fa fa-facebook-official" aria-hidden="true"></i>
-              <i class="fa fa-twitter-square" aria-hidden="true"></i>
-              <i class="fa fa-google-plus-square" aria-hidden="true"></i>
-            </div>
-            <div>
-              <p>NewLetter</p>
-              <input type="text" name="email" placeholder="Enter email" />
-              <button className="btn-dark">go</button>
+              <div className="Social_icons">
+                <h5 className="Social_title">Connect with me</h5>
+                <i class="fa fa-facebook-official" aria-hidden="true"></i>
+                <i class="fa fa-twitter-square" aria-hidden="true"></i>
+                <i class="fa fa-google-plus-square" aria-hidden="true"></i>
+              </div>
+              <div>
+                <p className="News_letter">NewLetter</p>
+                <input type="text" name="email" placeholder="Enter email" />
+                <button className="btn-dark">go</button>
+              </div>
             </div>
           </Col>
         </Row>
