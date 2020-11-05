1c2f14d64708296004da188a638ca39c7a61e441
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-11-05 00:02:08 -0800

    Commiting changes

--- src/screens/BlogScreen.js
+++ src/screens/BlogScreen.js
@@ -1,9 +1,37 @@
 import React from "react";
+import Links from "../components/Links";
+import { Col, Row } from "react-bootstrap";
+import Brideone from "../assets/Brideone.png";
+import Bridetwo from "../assets/Bridetwo.png";
 
 const Blog = () => {
   return (
     <div>
-      <h1>Blog</h1>
+      <Links />
+      <Row>
+        <Col>
+          <div>
+            <p>May 1, 2012 posted by: Jo This is just a place holder.</p>
+            <img src={Brideone} alt="img" />
+            <p>
+              This is just a place holder, so you can see what the site would
+              look like. This is just a place holder, so you can see what the
+              site would look like. - read more
+            </p>
+          </div>
+        </Col>
+        <Col>
+          <div>
+            <p>May 1, 2012 posted by: Jo This is just a place holder.</p>
+            <img src={Bridetwo} alt="img" />
+            <p>
+              This is just a place holder, so you can see what the site would
+              look like. This is just a place holder, so you can see what the
+              site would look like. - read more
+            </p>
+          </div>
+        </Col>
+      </Row>
     </div>
   );
 };
