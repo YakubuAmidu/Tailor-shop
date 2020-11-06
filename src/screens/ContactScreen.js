0bfbb0d04dd3503cd69e089189cd70e6783fae10
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-11-06 00:08:53 -0800

    Commiting changes

--- src/screens/ContactScreen.js
+++ src/screens/ContactScreen.js
@@ -9,12 +9,12 @@ const ContactScreen = () => {
       <Links />
       <div className="Contact_section">
         <h1 className="About_header">ContactScreen</h1>
-        <p className="About_header-paragraph">
+        <p className="Contact_header-paragraph">
           This is just a place holder, so you can see what the site would look
           like. This is just a place holder, so you can see what the site would
           look like.
         </p>
-        <p className="About_header-paragraph">
+        <p className="Contact_header-paragraph">
           This is just a place holder, so you can see what the site would look
           like. This is just a place holder, so you can see what the site would
           look like.
@@ -23,19 +23,15 @@ const ContactScreen = () => {
         <Row>
           <Col sm={6}>
             <div className="Contact_address">
-              <p className="Shop">SHOP DETAILS</p>
+              <p className="Shop_header">SHOP DETAILS</p>
+              <p className="Email">Email: company@email.com</p>
+              <p className="Phone_header">Phone: 987 654 3210, 987 654 3210</p>
+              <p className="Days">Hours: Monday to Friday: 9AM - 8PM</p>
               <p className="Shop_paragraph">
-                126-U This is just a place holder, so you can see what the site
-                would look like., 9183
+                Location:
+                <br />
+                1234 platinum street, NY, 90022;
               </p>
-              <p className="Email">EMAIL</p>
-              <p className="Email_address">company@email.com</p>
-              <p className="Phone_header">Call</p>
-              <p className="Number">987 654 3210</p>
-              <p lassName="Number">987 654 3210</p>
-              <p lassName="Number">987 654 3210</p>
-              <p lassName="Buiness">BUSINESS HOURS</p>
-              <p className="Days">Monday to Friday: 9AM - 8PM</p>
             </div>
           </Col>
           <Col sm={6}>
